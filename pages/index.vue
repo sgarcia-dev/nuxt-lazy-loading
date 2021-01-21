e3cf022ece266259bd71bf0dcae41e6d76627e8e
Sergei Garcia <sgarcia.dev@gmail.com>
2021-01-21 17:21:18 -0600

    typo

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">nuxt-lazy-loading</h1>
+      <h1 class="title">My Lazy Loading Nuxt App</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
